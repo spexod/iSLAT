7cc03115426aa46134c8f5823d555763caffbba0
banzatti <andreabanzatti@gmail.com>
2025-10-15 12:35:06 -0500

    Few fixes to Output Full Spectrum

--- iSLAT/Modules/FileHandling/OutputFullSpectrum.py
+++ iSLAT/Modules/FileHandling/OutputFullSpectrum.py
@@ -8,6 +8,7 @@ from tkinter import filedialog
 # import tkinter as tk
 
 from iSLAT.Modules.FileHandling import *
+import iSLAT.Modules.FileHandling.iSLATFileHandling as ifh
 
 
 def output_full_spectrum(islat_ref):
@@ -39,18 +40,19 @@ def output_full_spectrum(islat_ref):
                 plt.text(label_x, label_y, svd_species[i] + ' ' + svd_lineID[i]+' ', fontsize=6, rotation=90, va='top',
                         ha='left', color='grey')
 
+    # saved_lines = ifh.read_line_saves(file_name=self.islat.input_line_list)
+    # if saved_lines.empty:
 
     # read input spectrum
     cc = 2.99792458e5  # speed of light in km/s
 
-    RV = 15. # this should be taken from the GUI
     spectrum = pd.read_csv(spectrum_path, sep=',')
-    rv = islat_ref.molecules_dict._global_parms.get("stellar_rv", RV)
+    # take RV from the current global value
+    rv = islat_ref.molecules_dict.global_stellar_rv
 
-    waveCI = spectrum['wave']
-    waveCI = waveCI - (waveCI / cc * rv)
-    fluxCI = spectrum['flux']
-    offsetCI = 0.
+    wave = spectrum['wave']
+    wave = wave - (wave / cc * rv)
+    flux = spectrum['flux']
 
 
     plt.figure(figsize=figs)
@@ -65,19 +67,18 @@ def output_full_spectrum(islat_ref):
             mol_labels.append(mol.displaylabel)
             mol_colors.append(mol.color)
 
-    lw = 1.
-
     subplots = {}
 
     for n, xlim in enumerate(xlim1):
         # add a new subplot iteratively
         xr = [xlim1[n], xlim1[n]+step]
         subplots[n] = plt.subplot(len (xlim1), 1, n + 1)
-        maxv = np.nanmax(fluxCI[(waveCI > xr[0]-0.02) & (waveCI < xr[1])])
+        maxv = np.nanmax(flux[(wave > xr[0]-0.02) & (wave < xr[1])])
         ymax = maxv + maxv*ymaxfct
         #ymax = 0.12
         ymin = -0.005
         plt.xlim(xr)
+        plt.xticks (np.arange(xr[0], xr[1], 0.25))
         plt.ylim([ymin,ymax])
         plt.ylabel("Flux dens. (Jy)")
         plot_atomic(xr,ymin,ymax)
@@ -86,8 +87,8 @@ def output_full_spectrum(islat_ref):
 
         plot_renderer.render_main_spectrum_output(
             subplot = subplots[n],
-            wave_data = waveCI, 
-            flux_data = fluxCI + offsetCI,
+            wave_data = wave,
+            flux_data = flux,
             molecules = islat_ref.molecules_dict,
             summed_wavelengths = summed_wavelengths,
             summed_flux = summed_flux
